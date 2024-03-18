8850aaf6c1c590e4db89b6fad34fde54d77945c2
Dávid Komorowicz <dawars00@gmail.com>
2024-03-18 18:57:00 +0100

    Fix desc

--- colorization_benchmark/model_wrappers/deep_remaster.py
+++ colorization_benchmark/model_wrappers/deep_remaster.py
@@ -27,10 +27,10 @@ from third_party.deepremaster.model.remasternet import NetworkC
 
 class DeepRemaster(Colorizer):
     method_name = "deepremaster"
-    description = ("This model is originally designed for film colorization.\n"
-                   "To run this benchmark the input images is duplicated 5 times.\n"
-                   "The reference images are supposed to be colored frames chosen from the movies.\n"
-                   "This means that significant differences in the reference images cannot be used, as illustrated below.\n"
+    description = ("This model is originally designed for film colorization.\n\n"
+                   "To run this benchmark the input images is duplicated 5 times.\n\n"
+                   "The reference images are supposed to be colored frames chosen from the movies.\n\n"
+                   "This means that significant differences in the reference images cannot be used, as illustrated below.\n\n"
                    "Another interesting finding is that the temporal convolution, responsible for homigenizing the color "
                    "between conscutive frames, learned to color the sky and trees without reference."
                    )
