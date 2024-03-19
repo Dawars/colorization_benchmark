cc808b60fb42d3a72d736e706dec2ee3827a48c4
Dávid Komorowicz <dawars00@gmail.com>
2024-03-19 18:38:05 +0100

    Add hardware info

--- colorization_benchmark/utils/templating.py
+++ colorization_benchmark/utils/templating.py
@@ -1,6 +1,8 @@
 import datetime
 from pathlib import Path
 
+import torch
+
 from colorization_benchmark.model_wrappers.base_colorizer import BaseColorizer
 
 
@@ -28,6 +30,8 @@ last_modified_at: '{datetime.datetime.utcnow()}'
 # {pretty_print(benchmark_type)}
 ## {pretty_print(method_name)}
 
+Paper: [{colorizer.get_paper_link()}]()
+
 {colorizer.get_description(benchmark_type)}
 
 {header}
@@ -35,15 +39,29 @@ last_modified_at: '{datetime.datetime.utcnow()}'
 """
 
 
+def get_gpu_info():
+    gpu_info = ""
+    if torch.cuda.is_available():
+        gpu_device = torch.cuda.get_device_properties(0)
+        gpu_info = (f"{gpu_device.name} {round(gpu_device.total_memory / 1024 ** 3)} GB, "
+                    f"Compute Capability {gpu_device.major}.{gpu_device.minor}")
+    else:
+        gpu_info = "None"
+
+    return gpu_info
+
+
 def footer(method_name: str, benchmark_type: str, colorizer: BaseColorizer):
+    gpu_info = get_gpu_info()
     return '''
 ### Additional Information
 
 - Last updated: {{ "''' + str(datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")) + '''" | date: site.minima.date_format }}
-- Paper: [''' + colorizer.get_paper_link() + ''']()
+- GPU info: ''' + f"{gpu_info}" + '''
+- CUDA version: ''' + f"{torch.version.cuda}" + '''
+- PyTorch version: ''' + f"{torch.__version__}" + '''
 
-
-### Other categories:
+### Other Categories:
 
 {% for p in site.pages %}
 {% if p.tag == "''' + method_name + '''" and p.url != page.url %}
